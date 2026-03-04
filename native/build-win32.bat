23889fd7901ffb0178c665cf54273ae3a0d789ba
JoeRSPS <elaroofficial@gmail.com>
2026-03-04 15:41:12 -0700

    test

--- native/build-win32.bat
+++
@@ -1,2 +0,0 @@
-"C:\Program Files\CMake\bin\cmake.exe" -G "Visual Studio 16 2019" -A Win32 -S . -B "build32"
-"C:\Program Files\CMake\bin\cmake.exe" --build build32 --config Release
--- native/build-win64.bat
+++
@@ -1,2 +0,0 @@
-"C:\Program Files\CMake\bin\cmake.exe" -G "Visual Studio 16 2019" -A x64 -S . -B "build64"
-"C:\Program Files\CMake\bin\cmake.exe" --build build64 --config Release
